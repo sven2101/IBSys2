69113e8e3fb6ff4a94f89c52448f732c6a5f582f
MarashSC2 <marashsc@freenet.de>
2015-10-25 10:56:26 +0100

    Erster chart in der LagerComponent,
    Der Chart zeigt die Lagerwerte der Erzeugnisse in einem Balkendiagramm.

--- Project_AluGurke/src/app/components/lager/Lager.controller.ts
+++ Project_AluGurke/src/app/components/lager/Lager.controller.ts
@@ -7,6 +7,8 @@ class LagerController {
 	alleErzeugnisse:Array<NewErzeugnis>;
 	alleKaufTeile:Array<NewKaufTeil>;
 	
+	data: Array<number>;
+	
 	showErzeugnisse: boolean;
 	showKaufTeile: boolean;
 	
@@ -15,6 +17,14 @@ class LagerController {
 		this.alleKaufTeile = teileService.alleKaufteile;
 		this.showErzeugnisse = true;
 		this.showKaufTeile = true;
+		this.erzeugeData();
+	}
+	
+	erzeugeData(){
+		this.data = [];
+		for (var i = 0; i < this.alleErzeugnisse.length;i++) {
+			this.data.push(Math.round((this.alleErzeugnisse[i].lagerMenge * this.alleErzeugnisse[i].teileWert)*100)/100);
+		}
 	}
 }
 
--- Project_AluGurke/src/app/js/components/Lager/Lager.controller.js
+++ Project_AluGurke/src/app/js/components/Lager/Lager.controller.js
@@ -8,7 +8,14 @@ var LagerController = (function () {
         this.alleKaufTeile = teileService.alleKaufteile;
         this.showErzeugnisse = true;
         this.showKaufTeile = true;
+        this.erzeugeData();
     }
+    LagerController.prototype.erzeugeData = function () {
+        this.data = [];
+        for (var i = 0; i < this.alleErzeugnisse.length; i++) {
+            this.data.push(Math.round((this.alleErzeugnisse[i].lagerMenge * this.alleErzeugnisse[i].teileWert) * 100) / 100);
+        }
+    };
     return LagerController;
 })();
 angular.module('LagerModule').controller('LagerController', ['NewTeileService', LagerController]);
