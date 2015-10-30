8f647730e0180beb779d7e837c362cf9f7af8bda
MarashSC2 <marashsc@freenet.de>
2015-10-30 17:40:24 +0100

    Remove input and checkboxes

--- Project_AluGurke/src/app/components/appServices/ProgrammService.ts
+++ Project_AluGurke/src/app/components/appServices/ProgrammService.ts
@@ -14,20 +14,20 @@ class ProgrammService {
 
     erzeugeProgrammPositionen() {
         this.produktionsprogramm = [
-            new ProgrammPosition(1,1,0),
-            new ProgrammPosition(1,2,0),
-            new ProgrammPosition(1,3,0),
-            new ProgrammPosition(1,4,0),
+            new ProgrammPosition(1,1,100),
+            new ProgrammPosition(1,2,100),
+            new ProgrammPosition(1,3,100),
+            new ProgrammPosition(1,4,100),
 
-            new ProgrammPosition(2,1,0),
-            new ProgrammPosition(2,2,0),
-            new ProgrammPosition(2,3,0),
-            new ProgrammPosition(2,4,0),
+            new ProgrammPosition(2,1,100),
+            new ProgrammPosition(2,2,100),
+            new ProgrammPosition(2,3,100),
+            new ProgrammPosition(2,4,100),
 
-            new ProgrammPosition(3,1,0),
-            new ProgrammPosition(3,2,0),
-            new ProgrammPosition(3,3,0),
-            new ProgrammPosition(3,4,0),
+            new ProgrammPosition(3,1,100),
+            new ProgrammPosition(3,2,100),
+            new ProgrammPosition(3,3,100),
+            new ProgrammPosition(3,4,100),
         ];
     }
 
