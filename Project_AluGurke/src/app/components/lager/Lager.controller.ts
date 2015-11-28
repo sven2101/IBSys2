e12d3f27e64e6531de4ee690600117de685f8629
MarashSC2 <marashsc@freenet.de>
2015-11-28 14:30:25 +0100

    Lager tabs und sortieren

--- Project_AluGurke/src/app/components/lager/Lager.controller.ts
+++ Project_AluGurke/src/app/components/lager/Lager.controller.ts
@@ -4,27 +4,67 @@
 /// <reference path="../appServices/NewTeileService.ts" />
 
 class LagerController {
-	alleErzeugnisse:Array<NewErzeugnis>;
-	alleKaufTeile:Array<NewKaufTeil>;
-	
-	data: Array<number>;
-	
+	alleErzeugnisse: Array<NewErzeugnis>;
+	alleKaufTeile: Array<NewKaufTeil>;
+
 	showErzeugnisse: boolean;
 	showKaufTeile: boolean;
-	
-	constructor(teileService: NewTeileService){
+
+	constructor(teileService: NewTeileService) {
 		this.alleErzeugnisse = teileService.alleErzeugnisse;
 		this.alleKaufTeile = teileService.alleKaufteile;
 		this.showErzeugnisse = true;
-		this.showKaufTeile = true;
-		this.erzeugeData();
+		this.showKaufTeile = false;
 	}
-	
-	erzeugeData(){
-		this.data = [];
-		for (var i = 0; i < this.alleErzeugnisse.length;i++) {
-			this.data.push(Math.round((this.alleErzeugnisse[i].lagerMenge * this.alleErzeugnisse[i].teileWert)*100)/100);
-		}
+
+	toggleShowErzeugnisse(): void {
+		this.showKaufTeile = false;
+		this.showErzeugnisse = true;
+	}
+
+	toggleShowKaufTeile(): void {
+		this.showErzeugnisse = false;
+		this.showKaufTeile = true;
+	}
+
+	sortiereErzeugnisse(kriterium: string) {
+		this.sortiere(this.alleErzeugnisse, kriterium);
+	}
+
+	sortiereKaufTeile(kriterium: string) {
+		this.sortiere(this.alleKaufTeile, kriterium);
+	}
+
+	sortiere(array: Array<any>, kriterium: string): void {
+		array.sort(function(a: any, b: any) {
+			var differenz = 0;
+
+			if (a.hasOwnProperty(kriterium)) {
+				if (typeof a[kriterium] === 'boolean') {
+					differenz = a[kriterium] === b[kriterium] ? 0 : a[kriterium] ? -1 : 1;
+				} else if (kriterium === 'id') {
+					differenz = a.id - b.id;
+				}
+				else {
+					differenz = b[kriterium] - a[kriterium];
+				}
+			}
+
+			if (kriterium === 'summe') {
+				differenz = b.teileWert * b.lagerMenge - a.teileWert * a.lagerMenge;
+			}
+
+			if (differenz < 0) {
+				differenz = -1;
+			} else {
+				differenz = 1
+			}
+
+			if (differenz === 0) {
+				return a['id'] - b['id'];
+			}
+			return differenz;
+		});
 	}
 }
 
